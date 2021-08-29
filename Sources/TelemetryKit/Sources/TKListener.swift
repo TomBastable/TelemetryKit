724715f12b2e51746ba011279235df145784e000
Tom Bastable <bastable@gmail.com>
2021-08-29 16:54:37 +0100

    Update TKListener.swift

--- Sources/TelemetryKit/Sources/TKListener.swift
+++ Sources/TelemetryKit/Sources/TKListener.swift
@@ -25,7 +25,7 @@ public class TKListener: NSObject {
 	public var uiDelegate: TKUIDelegate?
 	public var voiceDelegate: TKVoiceDelegate?
 	public var acceptedPacketTypes: Set<TKPacketType>!
-	internal var delegate: TKDelegate!
+	var delegate: TKDelegate!
     private let liveSessionInfoQueue: DispatchQueue!
 	private var socket: GCDAsyncUdpSocket!
 	private var _liveSessionInfoUnsafe: TKLiveSessionInfo! // Do NOT access this directly!
